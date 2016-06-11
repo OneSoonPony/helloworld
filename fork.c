662d1f837f149215f3a1c3a3e10c75580141369b
OneSoonPony <onesoonpony@163.com>
2016-06-11 21:40:40 +0800

    Add files via upload

---
+++ fork.c
@@ -0,0 +1,33 @@
+#include <erron.h>
+#include <start.b>
+/*
+*this is fork function
+*(C) 2016 Pony OneSoon
+
+*/
+
+int get_mess(){
+
+
+return ERRON;
+
+/*   
+
+return -1 so that make sure the ponix system is runing well;
+
+
+
+*/
+
+}
+
+int main(){
+
+printf("the system is stating");
+
+
+
+shutdown();
+return 0;
+
+}
